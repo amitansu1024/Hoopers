48a7fa3f483fef6db499bac9d2539b381f2c9706
amitansu1024 <amitanshurajneti@gmail.com>
2023-05-24 14:23:08 +0530

    Better collision and score

--- Assets/Scripts/DetectCollision.cs
+++ Assets/Scripts/DetectCollision.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DetectCollision : MonoBehaviour
 {
+
+   //FIrstPersonHoops Hoops;
+    public GameObject ScoreBox;
+    public static int Score;
     // Start is called before the first frame update
     void Start()
     {
-        
+        ScoreBox = GameObject.Find("Score");
     }
 
     // Update is called once per frame
@@ -15,8 +20,11 @@ public class DetectCollision : MonoBehaviour
     {
     }
     void OnTriggerEnter(Collider other) { 
-        if (other.gameObject.name == "net") { 
+        if (other.gameObject.name == "ScoreHitbox") { 
             Debug.Log("SCored ball touched with net");
+            ScoreBox.GetComponent<TextMeshProUGUI>().text = "Score : "  + ++Score;
+            Destroy(other.gameObject.transform.parent.gameObject);
+            //Destroy(other.gameObject);
         }
     }
 }
